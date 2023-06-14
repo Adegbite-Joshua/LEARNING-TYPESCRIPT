e889e95388b4fd5efee5d18f08426ca7621d80cb
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-14 19:07:13 +0100

    Learning how to target value on an input

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 var nameOfSDtudent = 'adehfh';
 var numbers = 43;
-var one = document.getElementById('inp1');
+var one = Number(document.getElementById('inp1').value);
 var two = document.getElementById('inp2');
 var addNumbers = function () {
-    document.getElementById('output').innerHTML = '';
-    for (var i = 0; i < 122; i++) {
-        var tr = '';
-        for (var j = 0; j < 122; j++) {
-            tr += "<td>".concat(i * j, "</td>");
-        }
-        document.getElementById('output').innerHTML += tr;
-    }
+    // document.getElementById('output').innerHTML = ''
+    // for (let i = 0; i < 122; i++) {
+    //     let tr = ''
+    //     for (let j = 0; j < 122; j++) {
+    //         tr += `<td>${i*j}</td>`
+    //     }
+    //     document.getElementById('output').innerHTML += tr
+    // }
+    console.log(one);
 };
 var nameArray = [{ namee: 'dfff', class: 1 }, { namee: 'ehssh', class: 2 }, { namee: 'ms,ndj', class: 3 }, { namee: '.dkklkew', class: 4 }];
-nameArray.map(function (person) {
-    document.getElementById('nameDisplay').innerHTML += "<p>".concat(person.namee, "</p>");
-});
-nameArray.push('wertyu');
+// nameArray.map((person)=>{
+//     document.getElementById('nameDisplay').innerHTML += `<p>${person.namee}</p>`
+// })
 console.log(nameArray);
+// nameArray.push('wertyu')
--- script.ts
+++ script.ts
@@ -1,20 +1,22 @@
 let nameOfSDtudent:string = 'adehfh'
 let numbers:number = 43
-let one:any = document.getElementById('inp1')
+let one:any = Number((<HTMLInputElement>document.getElementById('inp1')).value)
 let two:any = document.getElementById('inp2')
 const addNumbers = ():any =>{
-    document.getElementById('output').innerHTML = ''
-    for (let i = 0; i < 122; i++) {
-        let tr = ''
-        for (let j = 0; j < 122; j++) {
-            tr += `<td>${i*j}</td>`
-        }
-        document.getElementById('output').innerHTML += tr
-    }
+    // document.getElementById('output').innerHTML = ''
+    // for (let i = 0; i < 122; i++) {
+    //     let tr = ''
+    //     for (let j = 0; j < 122; j++) {
+    //         tr += `<td>${i*j}</td>`
+    //     }
+    //     document.getElementById('output').innerHTML += tr
+    // }
+    console.log(one);
+    
 }
 let nameArray:object[] = [{namee:'dfff', class:1},{namee:'ehssh', class:2},{namee:'ms,ndj', class:3},{namee:'.dkklkew', class:4} ]
-nameArray.map((person)=>{
-    document.getElementById('nameDisplay').innerHTML += `<p>${person.namee}</p>`
-})
+// nameArray.map((person)=>{
+//     document.getElementById('nameDisplay').innerHTML += `<p>${person.namee}</p>`
+// })
 console.log(nameArray);
-nameArray.push('wertyu')
+// nameArray.push('wertyu')
