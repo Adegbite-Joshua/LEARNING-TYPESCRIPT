ae42448d30dd154c57e283de788317c20e57df18
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-18 19:51:57 +0100

    Worked on accepting operation type from select an option and performing operation based on it

--- script.js
+++ script.js
@@ -3,13 +3,50 @@ let numbers = 43;
 const addNumbers = () => {
     let one = parseFloat(document.getElementById('inp1').value);
     let two = parseFloat(document.getElementById('inp2').value);
+    let operation_type = document.getElementById('operation_type').value;
     document.getElementById('output').innerHTML = '';
-    for (let i = 0; i < one; i++) {
-        let tr = '';
-        for (let j = 0; j < two; j++) {
-            tr += `<td style="width: '100px'">${i * j}</td>`;
+    // for (let i = 0; i < one; i++) {
+    //     let tr = ''
+    //     for (let j = 0; j < two; j++) {
+    //         tr += `<td style="width: '100px'">${i*j}</td>`
+    //     }
+    //     document.getElementById('output').innerHTML += tr
+    // }
+    if (operation_type == 'add') {
+        for (let i = 0; i < one; i++) {
+            let tr = '';
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i + j}</td>`;
+            }
+            document.getElementById('output').innerHTML += tr;
         }
-        document.getElementById('output').innerHTML += tr;
+    }
+    else if (operation_type == 'sub') {
+        for (let i = 0; i < one; i++) {
+            let tr = '';
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i - j}</td>`;
+            }
+            document.getElementById('output').innerHTML += tr;
+        }
+    }
+    else if (operation_type == 'mul') {
+        for (let i = 0; i < one; i++) {
+            let tr = '';
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i * j}</td>`;
+            }
+            document.getElementById('output').innerHTML += tr;
+        }
+    }
+    else if (operation_type == 'div') {
+        for (let i = 0; i < one; i++) {
+            let tr = '';
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i / j}</td>`;
+            }
+            document.getElementById('output').innerHTML += tr;
+        }
     }
     // console.log(one, two);
 };
--- script.ts
+++ script.ts
@@ -3,14 +3,49 @@ let numbers:number = 43
 const addNumbers = ():any =>{
     let one:any = parseFloat((<HTMLInputElement>document.getElementById('inp1')).value)
     let two:any = parseFloat((<HTMLInputElement>document.getElementById('inp2')).value)
+    let operation_type = (<HTMLSelectElement>document.getElementById('operation_type')).value
     document.getElementById('output').innerHTML = ''
-    for (let i = 0; i < one; i++) {
-        let tr = ''
-        for (let j = 0; j < two; j++) {
-            tr += `<td style="width: '100px'">${i*j}</td>`
+    // for (let i = 0; i < one; i++) {
+    //     let tr = ''
+    //     for (let j = 0; j < two; j++) {
+    //         tr += `<td style="width: '100px'">${i*j}</td>`
+    //     }
+    //     document.getElementById('output').innerHTML += tr
+    // }
+    if (operation_type=='add') {
+        for (let i = 0; i < one; i++) {
+            let tr = ''
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i+j}</td>`
+            }
+            document.getElementById('output').innerHTML += tr
         }
-        document.getElementById('output').innerHTML += tr
+    } else if (operation_type=='sub') {
+        for (let i = 0; i < one; i++) {
+            let tr = ''
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i-j}</td>`
+            }
+            document.getElementById('output').innerHTML += tr
+        }
+    } else if (operation_type=='mul') {
+        for (let i = 0; i < one; i++) {
+            let tr = ''
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i*j}</td>`
+            }
+            document.getElementById('output').innerHTML += tr
+        }
+    } else if (operation_type=='div') {
+        for (let i = 0; i < one; i++) {
+            let tr = ''
+            for (let j = 0; j < two; j++) {
+                tr += `<td style="width: '100px'">${i/j}</td>`
+            }
+            document.getElementById('output').innerHTML += tr
+        }
     }
+    
     // console.log(one, two);
     
 }
